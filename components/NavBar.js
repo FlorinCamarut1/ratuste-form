f14a775c71c734b53f6a80329cdf452d84c55610
FlorinCamarut1 <95957482+FlorinCamarut1@users.noreply.github.com>
2023-02-28 16:59:08 +0200

    meniu cu scroll in jos

---
+++ components/NavBar.js
@@ -0,0 +1,71 @@
+import React from "react";
+import Link from "next/link";
+import { useState } from "react";
+import { MenuIcon, XCircleIcon } from "@heroicons/react/solid";
+
+function NavBar() {
+  const [navbar, setNavbar] = useState(false);
+  return (
+    <header>
+      <nav className=" w-full bg-gradient-to-r from-cyan-500 to-blue-500  top-0 left-0 right-0 z-10 ">
+        {/* ------mobile menu----------------- */}
+        <div className="justify-between px-10  md:items-center md:flex md:px-8">
+          <div className="flex py-5  md:block items-center justify-between ">
+            <div className="font-bold text-xl cursor-pointer ">
+              <Link href="/">
+                <h2>LOGO</h2>
+              </Link>
+            </div>
+            {/* button mobile-nav */}
+            <div className="md:hidden ">
+              <button
+                className=" p-1 text-white rounded-full hover:shadow-md active:scale-50 "
+                onClick={() => setNavbar(!navbar)}
+              >
+                {navbar ? (
+                  <XCircleIcon className="h-9 " />
+                ) : (
+                  <MenuIcon className="h-9 " />
+                )}
+              </button>
+            </div>
+          </div>
+          <div>
+            <div
+              className={` md:block md:pb-0 md:mt-0 ${
+                navbar ? "p-12 md:p-0 block" : "hidden"
+              }`}
+            >
+              <ul className="  text-white items-center justify-center md:flex">
+                <li className="pb-2 text-xl text-white py-2 md:px-6 text-center cursor-pointer active:scale-50 hover:shadow-md rounded-md ">
+                  <Link href="#overview" onClick={() => setNavbar(!navbar)}>
+                    Overview
+                  </Link>
+                </li>
+                <li className="pb-2 text-xl text-white py-2 md:px-6 text-center cursor-pointer active:scale-50 hover:shadow-md rounded-md">
+                  <Link href="#features" onClick={() => setNavbar(!navbar)}>
+                    Features
+                  </Link>
+                </li>
+                <li className="pb-2 text-xl text-white py-2 md:px-6 text-center cursor-pointer active:scale-50 hover:shadow-md rounded-md">
+                  <Link href="#pricing" onClick={() => setNavbar(!navbar)}>
+                    Pricing
+                  </Link>
+                </li>
+                <li className="px-4 text-xl text-white py-2 md:px-6 text-center cursor-pointer active:scale-50 hover:shadow-md rounded-md">
+                  <Link href="#contact" onClick={() => setNavbar(!navbar)}>
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          {/* End mobile menu */}
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default NavBar;
+//overview, features, pricing, contact
