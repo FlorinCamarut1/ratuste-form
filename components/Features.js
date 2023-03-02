23697df339f4c6363171b3359579388874a5759e
FlorinCamarut1 <95957482+FlorinCamarut1@users.noreply.github.com>
2023-03-02 16:33:13 +0200

    am adaugat alta componenta

---
+++ components/Features.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Features() {
+  return (
+    <section
+      id="features"
+      className="h-screen bg-slate-600  flex items-center justify-center"
+    >
+      <h1>Features</h1>
+    </section>
+  );
+}
+
+export default Features;
