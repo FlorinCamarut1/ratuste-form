0d0f47cc493c5a2463f1bdcf1bd6972323225f6d
FlorinCamarut1 <95957482+FlorinCamarut1@users.noreply.github.com>
2023-03-06 19:22:40 +0200

    nav bar am setat la 1001 sa se rupa

--- components/NavBar.js
+++ components/NavBar.js
@@ -12,8 +12,10 @@ function NavBar() {
   const [navbar, setNavbar] = useState(false);
   return (
     <nav>
-      <div className="flex md:inline justify-between items-center justify-center px-5 py-4 md:py-0 md:px-0">
-        <h1 className={`${roboto_mono.className} text-white text-2xl pr-4`}>
+      <div className="flex  justify-between items-center justify-center px-5 py-4 md:py-3 md:pl-9 md:pr-0 ">
+        <h1
+          className={`${roboto_mono.className} text-white text-3xl pr-4 md:p-0`}
+        >
           IntelligentForms
         </h1>
         <div className="md:hidden">
@@ -26,6 +28,35 @@ function NavBar() {
             ) : (
               <MenuIcon width={30} height={30} />
             )}
+          </button>
+        </div>
+        {/* nav cu meniu */}
+        <div
+          className={`hidden md:inline  space-x-7 text-lg text-white bg-bg-nav py-5 px-5 rounded-l-3xl ${roboto_mono.className} mt-1`}
+        >
+          <button>
+            <Link href="#footer" />
+            Home
+          </button>
+          <button>
+            <Link href="#footer" />
+            Features
+          </button>
+          <button>
+            <Link href="#footer" />
+            Subscription
+          </button>
+          <button>
+            <Link href="#footer" />
+            Contact
+          </button>
+          <button>
+            <Link href="#footer" />
+            Sign In
+          </button>
+          <button>
+            <Link href="#footer" />
+            Register
           </button>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,27 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "1001px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       colors: {
         "rata-bg": "#1A2A34",
         "rata-bg2": "#77BDDF",
+        "bg-nav": "#2A4452",
       },
     },
   },
